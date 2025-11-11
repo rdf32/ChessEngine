26ec0dc09ee8687ff704e340153d95888182f272
Rylie Fleckenstein <rylie.fleckenstein@trojans.dsu.edu>
2025-11-11 08:39:07 -0600

    updates

--- ChessEngine/ChessEngine.cpp
+++ ChessEngine/ChessEngine.cpp
@@ -9,22 +9,7 @@
 #define tricky_position "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 "
 #define killer_position "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
 #define cmk_position "r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 "
-//
-//const char* SquareNames[64] = {
-//        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
-//        "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-//        "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
-//        "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
-//        "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
-//        "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-//        "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-//        "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
-//};
-//
-//const char* PieceSymbols[2][6] = {
-//    { "P", "N", "B", "R", "Q", "K" },
-//    { "p", "n", "b", "r", "q", "k" }
-//};
+
 
 int main()
 {   
